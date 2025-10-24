8d76e9fb064af1704b4e358fe6a26fef7f6e3bb2
mbasagoitia <marika.basagoitia@gmail.com>
2025-10-24 10:16:19 -0400

    feature: added more parameters to ForecastRequest

--- ai-service/app/schemas/forecast_request.py
+++ ai-service/app/schemas/forecast_request.py
@@ -4,7 +4,17 @@ from typing import List, Dict
 # This sends data to the ai service, requesting predicted output for this configuration and weather
 # and/or optimal placement of the grid nodes
 class ForecastRequest(BaseModel):
+    # One of the four specificed regions
     region_id: str
+    # Recent weather data
     weather_data: List[Dict[str, float]]
+    # Current output
     current_output_mw: float
+    # Max capacity
     capacity_mw: float
+    # Terrain at a given placement, think about how to simulate this (by coords? region? randomly?)
+    terrain: str
+    # Distance from the city being powered
+    distance: float
+    # Transmission loss due to distance between generators
+    transmission_loss: float
